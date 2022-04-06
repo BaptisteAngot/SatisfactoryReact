cfa2316ce5218f2ffece8cdb22968c5f371bf421
ArthurBrunet <Arthur.brunet76@gmail.com>
2022-04-06 16:36:05 +0200

    parseJsonToString | test unit

--- __test__/parsing.test.js
+++ __test__/parsing.test.js
@@ -1,28 +1,217 @@
 const functions = require('../src/utils/parsing.js')
 
-test('parsing', () => {
-    // CODECATEGORY:888:TABLE =(ASSEMBLAGE)[PIED*4]+[BOIS*1];
-    let inputArticle = functions.parseInputToJson("CODECATEGORY:888:TABLE =(ASSEMBLAGE)[PIED*4]+[BOIS*1];");
-    let jsonArticle = {
-        codeArticle: "888",
-        codeCategory: "CODECATEGORY",
-        codeOperation: "ASSEMBLAGE",
-        articles: [
-            {
-                codeArticle: "PIED",
-                quantite: 4
-            },
-            {
-                codeArticle: "BOIS",
-                quantite: 1
-            }
-        ]
-    };
-    expect(inputArticle.codeArticle).toBe(jsonArticle.codeArticle);
-    expect(inputArticle.codeCategory).toBe(jsonArticle.codeCategory);
-    expect(inputArticle.codeOperation).toBe(jsonArticle.codeOperation);
-    expect(inputArticle.articles[0].codeArticle).toBe(jsonArticle.articles[0].codeArticle);
-    expect(inputArticle.articles[0].quantite).toBe(jsonArticle.articles[0].quantite);
-    expect(inputArticle.articles[1].codeArticle).toBe(jsonArticle.articles[1].codeArticle);
-    expect(inputArticle.articles[1].quantite).toBe(jsonArticle.articles[1].quantite);
-});
+describe('ParseStringToJson',() => {
+    test('parsing', () => {
+        // CODECATEGORY:888:TABLE =(ASSEMBLAGE)[PIED*4]+[BOIS*1];
+        let inputArticle = functions.parseStringToJson("CODECATEGORY:888 =(ASSEMBLAGE)[PIED*4]+[BOIS*1];");
+        let jsonArticle = {
+            codeArticle: "888",
+            codeCategory: "CODECATEGORY",
+            codeOperation: "ASSEMBLAGE",
+            articles: [
+                {
+                    codeArticle: "PIED",
+                    quantite: 4
+                },
+                {
+                    codeArticle: "BOIS",
+                    quantite: 1
+                }
+            ]
+        };
+        expect(inputArticle.codeArticle).toBe(jsonArticle.codeArticle);
+        expect(inputArticle.codeCategory).toBe(jsonArticle.codeCategory);
+        expect(inputArticle.codeOperation).toBe(jsonArticle.codeOperation);
+        expect(inputArticle.articles[0].codeArticle).toBe(jsonArticle.articles[0].codeArticle);
+        expect(inputArticle.articles[0].quantite).toBe(jsonArticle.articles[0].quantite);
+        expect(inputArticle.articles[1].codeArticle).toBe(jsonArticle.articles[1].codeArticle);
+        expect(inputArticle.articles[1].quantite).toBe(jsonArticle.articles[1].quantite);
+    });
+
+    test('2 elements par recette', () => {
+        // CODECATEGORY:888:TABLE =(ASSEMBLAGE)[PIED*4]+[BOIS*1];
+        let inputArticle1 = functions.parseStringToJson("CODECATEGORY:888 =(ASSEMBLAGE)[PIED*4]+[BOIS*1]+[BOIS*1];");
+        expect(inputArticle1).toBe("pas plus de 2 elements par recette");
+    });
+
+    test('erreur de syntax: (CodeOpération)', () => {
+        let inputArticle2 = functions.parseStringToJson("CODECATEGORY:888 =(ASSEMBLAGE[PIED*4][BOIS*1];");
+        expect(inputArticle2).toBe("erreur de syntax: (CodeOpération)");
+    });
+
+    test('erreur de syntax: [codeActicle*qty]', () => {
+        let inputArticle3 = functions.parseStringToJson("CODECATEGORY:888=(ASSEMBLAGE)PIED*4BOIS*1;");
+        expect(inputArticle3).toBe("erreur de syntax: [codeActicle*qty]");
+    });
+
+    test('(empty) erreur de syntax: [codeActicle*qty]', () => {
+        let inputArticle3 = functions.parseStringToJson("CODECATEGORY:888 =(ASSEMBLAGE)");
+        expect(inputArticle3).toBe("erreur de syntax: [codeActicle*qty]");
+    });
+
+    test('erreur de syntax: CODECATEGORY:CODEARTICLE', () => {
+        let inputArticle3 = functions.parseStringToJson("CODECATEGORY888 =(ASSEMBLAGE[PIED*4][BOIS*1];");
+        expect(inputArticle3).toBe("erreur de syntax: CODECATEGORY:CODEARTICLE");
+    });
+
+    test('erreur de syntax: \'=\'', () => {
+        let inputArticle3 = functions.parseStringToJson("CODECATEGORY:888 (ASSEMBLAGE[PIED*4][BOIS*1];");
+        expect(inputArticle3).toBe("erreur de syntax: '='");
+    });
+})
+
+
+/*
+* {
+            codeArticle: "888",
+            codeCategory: "CODECATEGORY",
+            codeOperation: "ASSEMBLAGE",
+            articles: [
+                {
+                    codeArticle: "PIED",
+                    quantite: 4
+                },
+                {
+                    codeArticle: "BOIS",
+                    quantite: 1
+                }
+            ]
+        }*/
+describe('ParseJsonToString',() => {
+    test('parsing', () => {
+        // CODECATEGORY:888:TABLE =(ASSEMBLAGE)[PIED*4]+[BOIS*1];
+        let inputArticle = functions.parseJsonToString({
+            codeArticle: "888",
+            codeCategory: "CODECATEGORY",
+            codeOperation: "ASSEMBLAGE",
+            articles: [
+                {
+                    codeArticle: "PIED",
+                    quantite: 4
+                },
+                {
+                    codeArticle: "BOIS",
+                    quantite: 1
+                }
+            ]
+        });
+        let jsonArticle = "CODECATEGORY:888=(ASSEMBLAGE)[PIED*4][BOIS*1];";
+        expect(inputArticle).toBe(jsonArticle);
+    });
+    test('attribut : codeArticle, introuvable', () => {
+        // CODECATEGORY:888:TABLE =(ASSEMBLAGE)[PIED*4]+[BOIS*1];
+        let inputArticle = functions.parseJsonToString({
+            codeCategory: "CODECATEGORY",
+            codeOperation: "ASSEMBLAGE",
+            articles: [
+                {
+                    codeArticle: "PIED",
+                    quantite: 4
+                },
+                {
+                    codeArticle: "BOIS",
+                    quantite: 1
+                }
+            ]
+        });
+        expect(inputArticle).toBe("attribut : codeArticle, introuvable");
+    });
+    test('attribut : codeCategory, introuvable', () => {
+        // CODECATEGORY:888:TABLE =(ASSEMBLAGE)[PIED*4]+[BOIS*1];
+        let inputArticle = functions.parseJsonToString({
+            codeArticle: "888",
+            codeOperation: "ASSEMBLAGE",
+            articles: [
+                {
+                    codeArticle: "PIED",
+                    quantite: 4
+                },
+                {
+                    codeArticle: "BOIS",
+                    quantite: 1
+                }
+            ]
+        });
+        expect(inputArticle).toBe("attribut : codeCategory, introuvable");
+    });
+    test('attribut : codeOperation, introuvable', () => {
+        // CODECATEGORY:888:TABLE =(ASSEMBLAGE)[PIED*4]+[BOIS*1];
+        let inputArticle = functions.parseJsonToString({
+            codeArticle: "888",
+            codeCategory: "CODECATEGORY",
+            articles: [
+                {
+                    codeArticle: "PIED",
+                    quantite: 4
+                },
+                {
+                    codeArticle: "BOIS",
+                    quantite: 1
+                }
+            ]
+        });
+        expect(inputArticle).toBe("attribut : codeOperation, introuvable");
+    });
+    test('plus de 2 elements', () => {
+        // CODECATEGORY:888:TABLE =(ASSEMBLAGE)[PIED*4]+[BOIS*1];
+        let inputArticle = functions.parseJsonToString({
+            codeArticle: "888",
+            codeCategory: "CODECATEGORY",
+            codeOperation: "ASSEMBLAGE",
+            articles: [
+                {
+                    codeArticle: "PIED",
+                    quantite: 4
+                },
+                {
+                    codeArticle: "BOIS",
+                    quantite: 1
+                },
+                {
+                    codeArticle: "BOIS",
+                    quantite: 1
+                }
+            ]
+        });
+        expect(inputArticle).toBe("plus de 2 elements");
+    });
+    test('attribut : articles, introuvable', () => {
+        // CODECATEGORY:888:TABLE =(ASSEMBLAGE)[PIED*4]+[BOIS*1];
+        let inputArticle = functions.parseJsonToString({
+            codeArticle: "888",
+            codeCategory: "CODECATEGORY",
+            codeOperation: "ASSEMBLAGE",
+            artic: [
+                {
+                    codeArticle: "PIED",
+                    quantite: 4
+                },
+                {
+                    codeArticle: "BOIS",
+                    quantite: 1
+                }
+            ]
+        });
+        expect(inputArticle).toBe("attribut : articles, introuvable");
+    });
+    test('les attributs dans la list d\'articles incorrect ( ex: articles:[{codeArticle:\"codeArticle\",quantite:10}] )', () => {
+        // CODECATEGORY:888:TABLE =(ASSEMBLAGE)[PIED*4]+[BOIS*1];
+        let inputArticle = functions.parseJsonToString({
+            codeArticle: "888",
+            codeCategory: "CODECATEGORY",
+            codeOperation: "ASSEMBLAGE",
+            articles: [
+                {
+                    codeArticdle: "PIED",
+                    quantite: 4
+                },
+                {
+                    codeArticle: "BOIS",
+                    quantite: 1
+                }
+            ]
+        });
+        expect(inputArticle).toBe("les attributs dans la list d'articles incorrect ( ex: articles:[{codeArticle:\"codeArticle\",quantite:10}] )");
+    });
+})
+
--- src/utils/parsing.js
+++ src/utils/parsing.js
@@ -1,36 +1,71 @@
 const functions = {
-    //CODECATEGORY:888:TABLE=(ASSEMBLAGE)[PIED*4][BOIS*1][BOIS*1];
-    parseInputToJson: (input) => {
+    //CODECATEGORY:888:TABLE=(ASSEMBLAGE)[PIED*4][BOIS*1];
+    parseStringToJson: (input) => {
         input = input.replace(" ", "");
         const regExpForOp = /\((.*?)\)/
         const regExpForArt = /\[(.*?)\]/g;
         let splitEqual = input.split("=");
+        if(splitEqual.length !== 2) {
+            return "erreur de syntax: '='";
+        }
         let metadata = splitEqual[0].split(":");
+        if(metadata.length !== 2) {
+            return "erreur de syntax: CODECATEGORY:CODEARTICLE";
+        }
         let recette = splitEqual[1];
         let op = recette.match(regExpForOp)
+        if(!op) {
+            return "erreur de syntax: (CodeOpération)"
+        }
         let listeMatch = recette.match(regExpForArt);
         let articlesQt = []
-        listeMatch.forEach(x => {
-            x = x.replace("[","");
-            x = x.replace("]","");
-            articlesQt.push(x)
-        });
-
+        try {
+            listeMatch.map(x => {
+                x = x.replace("[","");
+                x = x.replace("]","");
+                let itemArray = x.split("*");
+                articlesQt.push({
+                    codeArticle: itemArray[0],
+                    quantite: parseInt(itemArray[1])
+                })
+            });
+        }catch (e){
+            return "erreur de syntax: [codeActicle*qty]";
+        }
+        if(listeMatch.length > 2) {
+            return "pas plus de 2 elements par recette";
+        }
         return {
             codeArticle: metadata[1],
             codeCategory: metadata[0],
             codeOperation: op[1],
-            articles: [
-                {
-                    codeArticle: articlesQt[0].split("*")[0],
-                    quantite: parseInt(articlesQt[0].split("*")[1])
-                },
-                {
-                    codeArticle: articlesQt[1].split("*")[0],
-                    quantite: parseInt(articlesQt[1].split("*")[1])
-                }
-            ]
+            articles: articlesQt
         };
+    },
+    parseJsonToString: (json) => {
+        if (!json.codeArticle){ return "attribut : codeArticle, introuvable"; }
+        if (!json.codeCategory){ return "attribut : codeCategory, introuvable"; }
+        if (!json.codeOperation){ return "attribut : codeOperation, introuvable"; }
+        if (!json.articles){ return "attribut : articles, introuvable"; }
+        if(json.articles.length > 2){ return "plus de 2 elements"; }
+        // CODECATEGORY:888
+        let metaData = json.codeCategory + ":" + json.codeArticle;
+        //(ASSEMBLAGE)
+        let op = "("+json.codeOperation+")";
+        //[PIED*4]+[BOIS*1]
+        let articles = "";
+        let error;
+        json.articles.forEach(item => {
+            if (!item.codeArticle || !item.quantite){
+                error = true;
+            }
+            articles+="["+item.codeArticle+"*"+item.quantite.toString()+"]";
+        });
+        if (error) {
+            return "les attributs dans la list d'articles incorrect ( ex: articles:[{codeArticle:\"codeArticle\",quantite:10}] )";
+        }
+        // CODECATEGORY:888:TABLE=(ASSEMBLAGE)[PIED*4][BOIS*1];
+        return metaData+"="+op+articles+";";
     },
 }
 
